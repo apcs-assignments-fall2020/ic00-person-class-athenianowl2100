78da192549beb447de0c46b7fb73611347ab854a
athenianowl2100 <nathanlintest@gmail.com>
2020-10-28 13:32:48 +0000

    First draft

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,40 @@
 public class Person {
-    
+    // Instance variables
+    private String firstName;
+    private String lastName;
+
+    // Constructor
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // doSomething()
+    public void doSomething() {
+        System.out.println("I'm watching Netflix");
+    }
+
+    // toString()
+    public String toString() {
+        String str = firstName + " " + lastName;
+        return str;
+    }
+
+    // getters
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    // setters
+    public void setFirstName(String firstName) {
+        if (firstName.trim().length() != 0) {
+            this.firstName = firstName;
+        }
+    }
 }
 
 
--- src/main/java/PersonClient.java
+++ src/main/java/PersonClient.java
@@ -1,5 +1,13 @@
 public class PersonClient {
     public static void main(String[] args) {
-        
+        Person p1 = new Person("Isabella", "Smith");
+        System.out.println(p1.getFirstName()); // Isabella
+        p1.setFirstName("  ");
+        System.out.println(p1.getFirstName()); // Bella
+
+        // Person p1 = new Person("Isabella", "Smith");
+        // System.out.println(p1.firstName); // Isabella
+        // p1.firstName = "";
+        // System.out.println(p1.firstName); // Bella
     }
 }
